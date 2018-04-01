542f8b3773a4fade065a400529d202f07c7d1067
Al <xochipillitx@gmail.com>
2018-04-01 02:44:52 -0500

    Added photo results into a carousel

--- js/main.js
+++ js/main.js
@@ -7,22 +7,27 @@ $("button").each(function() {
 
 function getFlickrJSON(buttonText) {
   var buttonValue = buttonText
-  var flickrURL = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=3c4d06636e9aab9e37cce43ac03a4544&per_page=10&format=json&text=medieval " + buttonValue + "&nojsoncallback=1&sort=interestingness-desc"
-  $.getJSON(flickrURL, function(){
-    $("#photos").empty();
-  })
-    .done(function(val) {
-      var result = val.photos.photo;
-      $.each(result, function(i, val) {
-        var farmId = result[i].farm;
-        var serverId = result[i].server;
-        var id = result[i].id;
-        var secret = result[i].secret;
-        var photoURL = "https://farm" + farmId + ".staticflickr.com/" + serverId + "/" + id + "_" + secret + ".jpg"
-        $("#photos").append("<img src=" + photoURL + "/>");
-      });
+  var flickrURL = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=3c4d06636e9aab9e37cce43ac03a4544&format=json&text=medieval " + buttonValue + "&nojsoncallback=1&sort=interestingness-desc"
+  $.getJSON(flickrURL, function() {
+      $("#carouselInner").empty();
+      $("#carouselInner").append("<div class='carousel-item active'>" +
+      "<img class='w-100 h-100' src='#' />" +
+        "</div>");
     })
-    .fail(function() {
-      console.log("error");
-    });
-}
+    .done(function(val) {
+        var result = val.photos.photo;
+        $.each(result, function(i, val) {
+            var farmId = result[i].farm;
+            var serverId = result[i].server;
+            var id = result[i].id;
+            var secret = result[i].secret;
+            var photoURL = "https://farm" + farmId + ".staticflickr.com/" + serverId + "/" + id + "_" + secret + "_b.jpg"
+            $("#carouselInner").append("<div class='carousel-item w-100 h-100'>" +
+            "<img class='w-100 h-100' src='" + photoURL +"' />" +
+              "</div>");
+            });
+        })
+      .fail(function() {
+        console.log("error");
+      });
+    }
