74fcd2edde111ea39445aedb2c38916079d7a91b
Al <xochipillitx@gmail.com>
2018-04-01 17:53:07 -0500

    Configured media player and its overall basic look.

--- js/main.js
+++ js/main.js
@@ -37,6 +37,10 @@ $(document).ready(function() {
 
   // Music API //
 
+  // Array WIthin Array
+
+  var testList = {}
+
   function getFreesoundJSON(buttonText) {
     var buttonValue = buttonText
     var soundURL = "https://freesound.org/apiv2/search/text/?token=opEsO9zEL9n9vCfBDwgleOGT9EpXXs31r9h2SJSz&fields=name,previews,duration&sort=score&filter=duration:[60 TO *]&query=medieval,music," + buttonText
@@ -44,24 +48,89 @@ $(document).ready(function() {
 
   // Media Player
 
-  var musicList = {
-    title: "Not Bubble Bubble",
-    m4a: "http://jplayer.org/audio/mp3/Miaow-07-Bubble.mp3",
-    oga: "http://jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
-  }
-
-  $("#jquery_jplayer_1").jPlayer({
-    ready: function(event) {
-      $(this).jPlayer("setMedia", musicList);
+  var musicList = [
+    {
+      title:"whaddafuk",
+      mp3:"http://www.jplayer.org/audio/mp3/TSP-01-Cro_magnon_man.mp3",
+      oga:"http://www.jplayer.org/audio/ogg/TSP-01-Cro_magnon_man.ogg"
     },
-    swfPath: "http://jplayer.org/latest/dist/jplayer",
-    supplied: "mp3, oga",
-    wmode: "window",
-    useStateClassSkin: true,
-    autoBlur: false,
-    smoothPlayBar: true,
-    keyEnabled: true,
-    remainingDuration: true,
-    toggleDuration: true
-  })
+    {
+      title:"Your Face",
+      mp3:"http://www.jplayer.org/audio/mp3/TSP-05-Your_face.mp3",
+      oga:"http://www.jplayer.org/audio/ogg/TSP-05-Your_face.ogg"
+    },
+    {
+      title:"Cyber Sonnet",
+      mp3:"http://www.jplayer.org/audio/mp3/TSP-07-Cybersonnet.mp3",
+      oga:"http://www.jplayer.org/audio/ogg/TSP-07-Cybersonnet.ogg"
+    },
+    {
+      title:"Tempered Song",
+      mp3:"http://www.jplayer.org/audio/mp3/Miaow-01-Tempered-song.mp3",
+      oga:"http://www.jplayer.org/audio/ogg/Miaow-01-Tempered-song.ogg"
+    },
+    {
+      title:"Hidden",
+      mp3:"http://www.jplayer.org/audio/mp3/Miaow-02-Hidden.mp3",
+      oga:"http://www.jplayer.org/audio/ogg/Miaow-02-Hidden.ogg"
+    },
+    {
+      title:"Lentement",
+      free:true,
+      mp3:"http://www.jplayer.org/audio/mp3/Miaow-03-Lentement.mp3",
+      oga:"http://www.jplayer.org/audio/ogg/Miaow-03-Lentement.ogg"
+    },
+    {
+      title:"Lismore",
+      mp3:"http://www.jplayer.org/audio/mp3/Miaow-04-Lismore.mp3",
+      oga:"http://www.jplayer.org/audio/ogg/Miaow-04-Lismore.ogg"
+    },
+    {
+      title:"The Separation",
+      mp3:"http://www.jplayer.org/audio/mp3/Miaow-05-The-separation.mp3",
+      oga:"http://www.jplayer.org/audio/ogg/Miaow-05-The-separation.ogg"
+    },
+    {
+      title:"Beside Me",
+      mp3:"http://www.jplayer.org/audio/mp3/Miaow-06-Beside-me.mp3",
+      oga:"http://www.jplayer.org/audio/ogg/Miaow-06-Beside-me.ogg"
+    },
+    {
+      title:"Bubble",
+      free:true,
+      mp3:"http://www.jplayer.org/audio/mp3/Miaow-07-Bubble.mp3",
+      oga:"http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
+    },
+    {
+      title:"Stirring of a Fool",
+      mp3:"http://www.jplayer.org/audio/mp3/Miaow-08-Stirring-of-a-fool.mp3",
+      oga:"http://www.jplayer.org/audio/ogg/Miaow-08-Stirring-of-a-fool.ogg"
+    },
+    {
+      title:"Partir",
+      free: true,
+      mp3:"http://www.jplayer.org/audio/mp3/Miaow-09-Partir.mp3",
+      oga:"http://www.jplayer.org/audio/ogg/Miaow-09-Partir.ogg"
+    },
+    {
+      title:"Thin Ice",
+      mp3:"http://www.jplayer.org/audio/mp3/Miaow-10-Thin-ice.mp3",
+      oga:"http://www.jplayer.org/audio/ogg/Miaow-10-Thin-ice.ogg"
+    }
+  ]
+
+  // Playlist
+  new jPlayerPlaylist({
+  jPlayer: "#jquery_jplayer_1",
+  cssSelectorAncestor: "#jp_container_1"
+}, musicList, {
+  swfPath: "http://jplayer.org/latest/dist/jplayer",
+  supplied: "oga, mp3",
+  wmode: "window",
+  useStateClassSkin: true,
+  autoBlur: false,
+  smoothPlayBar: true,
+  keyEnabled: true
+});
+
 });
