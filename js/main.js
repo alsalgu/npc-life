69d02923dc285edec676fcbe9e89df7a87350180
Al <xochipillitx@gmail.com>
2018-04-01 02:05:25 -0500

    JS-wise, button click now wipes previous results then adds new better pics

--- js/main.js
+++ js/main.js
@@ -7,8 +7,10 @@ $("button").each(function() {
 
 function getFlickrJSON(buttonText) {
   var buttonValue = buttonText
-  var flickrURL = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=3c4d06636e9aab9e37cce43ac03a4544&per_page=10&format=json&text=" + buttonValue + "&nojsoncallback=1"
-  $.getJSON(flickrURL)
+  var flickrURL = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=3c4d06636e9aab9e37cce43ac03a4544&per_page=10&format=json&text=medieval " + buttonValue + "&nojsoncallback=1&sort=interestingness-desc"
+  $.getJSON(flickrURL, function(){
+    $("#photos").empty();
+  })
     .done(function(val) {
       var result = val.photos.photo;
       $.each(result, function(i, val) {
@@ -17,7 +19,7 @@ function getFlickrJSON(buttonText) {
         var id = result[i].id;
         var secret = result[i].secret;
         var photoURL = "https://farm" + farmId + ".staticflickr.com/" + serverId + "/" + id + "_" + secret + ".jpg"
-        console.log(photoURL);
+        $("#photos").append("<img src=" + photoURL + "/>");
       });
     })
     .fail(function() {
