9ca085fa06bc912e2bc7da6e1f3ad4503077a744
Al <xochipillitx@gmail.com>
2018-04-01 03:17:11 -0500

    Size and privacy change for flickr results

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $("button").each(function() {
 
 function getFlickrJSON(buttonText) {
   var buttonValue = buttonText
-  var flickrURL = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=3c4d06636e9aab9e37cce43ac03a4544&format=json&text=medieval " + buttonValue + "&nojsoncallback=1&sort=interestingness-desc"
+  var flickrURL = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=3c4d06636e9aab9e37cce43ac03a4544&format=json&text=medieval " + buttonValue + "&nojsoncallback=1&sort=interestingness-desc&privacy_filter=1"
   $.getJSON(flickrURL, function() {
       $("#carouselInner").empty();
       $("#carouselInner").append("<div class='carousel-item active text-center'>" +
@@ -21,9 +21,9 @@ function getFlickrJSON(buttonText) {
             var serverId = result[i].server;
             var id = result[i].id;
             var secret = result[i].secret;
-            var photoURL = "https://farm" + farmId + ".staticflickr.com/" + serverId + "/" + id + "_" + secret + "_b.jpg"
-            $("#carouselInner").append("<div class='carousel-item w-100 h-100'>" +
-            "<img class='w-100 h-100' src='" + photoURL +"' />" +
+            var photoURL = "https://farm" + farmId + ".staticflickr.com/" + serverId + "/" + id + "_" + secret + ".jpg"
+            $("#carouselInner").append("<div class='carousel-item text-center w-100 h-100'>" +
+            "<img class='w-100' src='" + photoURL +"' />" +
               "</div>");
             });
         })
